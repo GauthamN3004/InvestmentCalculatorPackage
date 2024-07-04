55b2aeebab9b3a6ef39915cb5720c5250e004032
GauthamN3004 <gauthamn3004@gmail.com>
2024-07-04 20:55:11 +0530

    fixed the SIP calculation formula

--- build/lib/investment_calculator_package/SIP.py
+++ build/lib/investment_calculator_package/SIP.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import math
 
 class SIP_Calculator():
     def __init__(self):
@@ -6,17 +7,17 @@ class SIP_Calculator():
 
     def validate_dataframe(self, dataframe):
         if not isinstance(dataframe, pd.DataFrame):
-            raise("Not a pandas dataframe.")
+            raise Exception("Not a pandas dataframe.")
         
         df_columns = dataframe.columns
         required_columns = ['amount', 'rate_of_interest', 'number_of_years']
         
         if len(df_columns) != 3:
-            raise("The dataframe should contain 3 columns (amount, rate_of_interest, number_of_years) - found {0}".format(len(df_columns)))
+            raise Exception("The dataframe should contain 3 columns (amount, rate_of_interest, number_of_years) - found {0}".format(len(df_columns)))
 
         for column in required_columns:
             if column not in df_columns:
-                raise("The dataframe is missing '{0}' column. Following columns were found - {1}".format(column, str(list(df_columns))))
+                raise Exception("The dataframe is missing '{0}' column. Following columns were found - {1}".format(column, str(list(df_columns))))
 
         # check if all values are numeric
         try:
@@ -24,7 +25,7 @@ class SIP_Calculator():
             pd.to_numeric(dataframe['rate_of_interest'])
             pd.to_numeric(dataframe['number_of_years'])
         except:
-            raise("The values are not numeric.")
+            raise Exception("The values are not numeric.")
 
         return
 
@@ -37,4 +38,18 @@ class SIP_Calculator():
         dataframe = pd.read_csv(csv_location, header = header)
         self.validate_dataframe(dataframe)
         self.investment_dataframe = dataframe
+
+        total_investment_term = self.investment_dataframe['number_of_years'].sum()
+        final_investment_amount, current_year = 0, 0
+
+        for index, row in self.investment_dataframe.iterrows():
+            amount, rate_perc, years = row
+            current_year += years
+            monthly_compounding_rate = 1 + float(rate_perc) / 1200
+            yearly_compounding_rate = 1 + float(rate_perc) / 100
+            cur_investment = amount * monthly_compounding_rate * (math.pow(monthly_compounding_rate, years * 12) - 1) / (monthly_compounding_rate - 1)
+            final_investment_amount += (cur_investment * math.pow(yearly_compounding_rate, total_investment_term - current_year))
+            
+        print(final_investment_amount)
+        
         return
